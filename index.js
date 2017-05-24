7646735bd99de1a6cd02abcc25c55ad806410c88
Steven Liss <steven@stevenliss.com>
2017-05-24 22:49:33 +0000

    added config file to make separate dev/test/prod environments easier to maintain

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const { MongoClient } = require('mongodb')
 const api = require('./src/api')
+const config = require('./config')
 
-MongoClient.connect('mongodb://localhost:27017/tigerpack')
-  .then(db => api(db).listen(3000))
+MongoClient.connect(`mongodb://localhost:27017/${config.db}`)
+  .then(db => api(db).listen(config.port))
   .catch(error => console.error(error.stack))
